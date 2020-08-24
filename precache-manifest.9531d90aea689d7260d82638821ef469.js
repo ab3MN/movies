d24608be09c6da2fda87239e082e5c39ffd2dcca
ab3MN <ab3mn3@gmail.com>
2020-08-24 16:21:11 +0300

    Updates

---
+++ precache-manifest.9531d90aea689d7260d82638821ef469.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed255c9ab42f2886251e0e31f9e37df",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "5efc5408df23074be239",
+    "url": "/movies/static/css/8.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "b8ddbedee9b8ea02db93",
+    "url": "/movies/static/js/0.e2f2946c.chunk.js"
+  },
+  {
+    "revision": "5efc5408df23074be239",
+    "url": "/movies/static/js/8.6f03fa05.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies/static/js/8.6f03fa05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a6df40b2d60e2a85cee",
+    "url": "/movies/static/js/cast-page.0af89b86.chunk.js"
+  },
+  {
+    "revision": "14f27115165c6dac872b",
+    "url": "/movies/static/js/home-page.ea58e22a.chunk.js"
+  },
+  {
+    "revision": "dc3ec81e522c101fa235",
+    "url": "/movies/static/js/main.a6674796.chunk.js"
+  },
+  {
+    "revision": "5164c606cf5200853b76",
+    "url": "/movies/static/js/movieDetails-page.888839ef.chunk.js"
+  },
+  {
+    "revision": "a0f53c9de1d9ab7b53d7",
+    "url": "/movies/static/js/movies-page.2a5b4fb6.chunk.js"
+  },
+  {
+    "revision": "2351d79531d2810ddcde",
+    "url": "/movies/static/js/reviews-page.55e0fe9a.chunk.js"
+  },
+  {
+    "revision": "41288051eb80cb35bf83",
+    "url": "/movies/static/js/runtime-main.012da4b7.js"
+  }
+]);
